5f62ad9c49a379971380098517be1635c4f206ff
Romario Jashari <rxjash@gmail.com>
2023-03-01 09:47:19 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://romar.io',
-  integrations: [sitemap()],
+  integrations: [sitemap(), preact()]
 });
